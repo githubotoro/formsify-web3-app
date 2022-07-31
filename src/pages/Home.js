ef51d9dbff9316968e71f2ee9a4aefeaf2c25820
githubotoro <yupuday.git@gmail.com>
2022-07-31 13:37:31 +0530

    home-update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
 
 							<div className="mt-8 md:mt-10">
 								<button
-									className="btn btn-primary btn-sm h-fit w-fit px-2 py-0 sm:px-3 md:px-3 lg:px-5 sm:py-1 md:py-2 lg:py-3 text-md sm:text-lg md:text-xl lg:text-2xl items-center rounded-md sm:rounded-lg md:rounded-xl lg:rounded-2xl font-black shadow-lg"
+									className="btn btn-primary btn-sm h-fit w-fit px-3 lg:px-5 py-2 lg:py-3 text-xl lg:text-2xl items-center rounded-xl lg:rounded-2xl font-black shadow-lg"
 									onClick={() => {
 										navigate(`/dashboard`);
 									}}
@@ -98,18 +98,22 @@ const Home = () => {
 						<div className="text-md sm:text-lg mt-2">
 							You don't need us to interact with your Forms.
 							<br />
-							They are stored on{" "}
+							They are stored on blockchain and have their{" "}
 							<span className="kbd kbd-sm text-md sm:text-lg">
-								Blockchain
+								own
 							</span>{" "}
-							and have their own digital identity!
+							digital identity!
 						</div>
 					</center>
 				</div>
 
 				<div className="flex flex-col lg:flex-row px-2 sm:px-4 bg-base-200 py-4 -mt-2 sm:mt-0">
-					<div className="px-1 grid flex-grow h-32 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
+					<div className="px-1 grid flex-grow h-36 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
 						<center>
+							<span className="font-black badge badge-error rounded-lg text-lg sm:text-xl p-4 shadow-md">
+								Without Formsify
+							</span>
+							<br />
 							The <span className="font-black">SERVER</span> you
 							are trying to Contact 📡
 							<br /> Is currently{" "}
@@ -123,17 +127,21 @@ const Home = () => {
 					<div className="divider lg:divider-horizontal font-black text-xl">
 						vs
 					</div>
-					<div className="px-1 grid flex-grow h-32 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
+					<div className="px-1 grid flex-grow h-36 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
 						<center>
+							<span className="font-black badge badge-success rounded-lg text-lg sm:text-xl p-4 shadow-md">
+								With Formsify
+							</span>
+							<br />
 							Our Forms are{" "}
 							<span className="font-black">DECENTRALIZED</span> 📌
 							<br />
 							Even&nbsp;
 							<span className="font-black">IF</span>&nbsp;we go
-							down 👀
+							Down 👀
 							<br />
 							<span className="font-extrabold ">
-								They are still accessible, without us! ✅
+								They are still accessible, without Us! ✅
 							</span>
 						</center>
 					</div>
@@ -163,8 +171,12 @@ const Home = () => {
 				</div>
 
 				<div className="flex flex-col lg:flex-row px-2 sm:px-4 bg-base-200 py-4 -mt-2 sm:mt-0">
-					<div className="px-1 grid flex-grow h-32 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
+					<div className="px-1 grid flex-grow h-36 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
 						<center>
+							<span className="font-black badge badge-error rounded-lg text-lg sm:text-xl p-4 shadow-md">
+								Without Formsify
+							</span>
+							<br />
 							Let's <span className="font-black">HACK</span>
 							&nbsp;& alter those Forms 🤖
 							<br />
@@ -179,8 +191,12 @@ const Home = () => {
 					<div className="divider lg:divider-horizontal font-black text-xl">
 						vs
 					</div>
-					<div className="px-1 grid flex-grow h-32 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
+					<div className="px-1 grid flex-grow h-36 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
 						<center>
+							<span className="font-black badge badge-success rounded-lg text-lg sm:text-xl p-4 shadow-md">
+								With Formsify
+							</span>
+							<br />
 							Well, it will take{" "}
 							<span className="font-black">
 								10^24 (Septillion)
@@ -201,13 +217,125 @@ const Home = () => {
 		);
 	};
 
+	const formsifyCaptchaFree = () => {
+		return (
+			<>
+				<div className="flex flex-col justify-center items-center bg-base-200 px-4">
+					<center>
+						<div className="text-3xl sm:text-4xl md:text-5xl font-black drop-shadow-sm">
+							Captcha-Free
+						</div>
+						<div className="text-md sm:text-lg mt-2">
+							Do your forms require Captcha protection?
+							<br />
+							We ensure they are captcha-free and still{" "}
+							<span className="kbd kbd-sm text-md sm:text-lg">
+								prevent
+							</span>{" "}
+							spamming!
+						</div>
+					</center>
+				</div>
+
+				<div className="flex flex-col lg:flex-row px-2 sm:px-4 bg-base-200 py-4 -mt-2 sm:mt-0">
+					<div className="px-1 grid flex-grow h-36 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
+						<center>
+							<span className="font-black badge badge-error rounded-lg text-lg sm:text-xl p-4 shadow-md">
+								Without Formsify
+							</span>
+							<br />
+							Here comes that annoying&nbsp;
+							<span className="font-black">CAPTCHA</span>&nbsp;😩
+							<br /> Captcha took&nbsp;
+							<span className="font-black">TIME</span>
+							&nbsp;& deadline passed 😭
+							<br />
+							<span className="font-extrabold ">
+								I Ain't a Bot, I promise! 🥺
+							</span>
+						</center>
+					</div>
+					<div className="divider lg:divider-horizontal font-black text-xl">
+						vs
+					</div>
+					<div className="px-1 grid flex-grow h-36 w-full card bg-base-300 rounded-box place-items-center shadow-md text-md sm:text-lg font-bold">
+						<center>
+							<span className="font-black badge badge-success rounded-lg text-lg sm:text-xl p-4 shadow-md">
+								With Formsify
+							</span>
+							<br />
+							Each form submit is backed by&nbsp;
+							<span className="font-black">GAS</span>&nbsp;⛽
+							<br />
+							Only&nbsp;
+							<span className="font-black">SUPER-RICH</span>
+							&nbsp;bots can spam the form 💰
+							<br />
+							<span className="font-extrabold ">
+								See yaa later, Bots! 👋
+							</span>
+						</center>
+					</div>
+				</div>
+			</>
+		);
+	};
+
+	const formsifyContactUs = () => {
+		return (
+			<>
+				<div className="hero bg-base-200 -mt-12 -mb-8">
+					<div className="hero-content flex-col lg:flex-row-reverse">
+						<div className="p-10 text-center">
+							<div className="text-3xl sm:text-4xl md:text-5xl font-black drop-shadow-sm">
+								Contact Us! 👋
+							</div>
+							<div className="py-6 font-semibold sm:text-lg">
+								Drop an&nbsp;
+								<span className="font-bold">Email</span>&nbsp;📧
+								or&nbsp;
+								<span className="font-bold">Schedule</span>
+								&nbsp;a 30 min one-to-one meeting 📡 directly
+								with&nbsp;
+								<span className="font-bold">Us</span>
+								&nbsp;via Calendly!
+							</div>
+							<div className="-mt-3 space-x-3 space-y-3">
+								<a
+									className="btn btn-accent btn-sm h-fit w-fit px-1 lg:px-3 py-1 lg:py-2 text-lg lg:text-xl items-center rounded-xl lg:rounded-2xl font-black shadow-lg"
+									href="https://calendly.com/uday-khokhariya/30min"
+									target="_blank"
+									rel="noreferrer"
+									type="button"
+								>
+									Schedule a Meeting
+								</a>
+								<a
+									className="btn btn-accent btn-sm h-fit w-fit px-1 lg:px-3 py-1 lg:py-2 text-lg lg:text-xl items-center rounded-xl lg:rounded-2xl font-black shadow-lg"
+									href="mailto: formsifying@gmail.com"
+									type="button"
+								>
+									Send Email
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	};
+
 	return (
 		<>
 			{formsifyHero()}
 			<div className="blankDiv pt-5 bg-base-200" />
+			{formsifyCaptchaFree()}
+			<div className="blankDiv pt-5 bg-base-200" />
 			{formsifySelfReliant()}
 			<div className="blankDiv pt-5 bg-base-200" />
 			{formsifyImmutable()}
+			<div className="blankDiv pt-5 bg-base-200" />
+			{formsifyContactUs()}
 			<div className="blankDiv pt-5 bg-base-200" />
 		</>
 	);
